f1d4b9fb4dd2c7685b9779f23cd0b83924b1ba11
mark <h091237557@gmail.com>
2015-09-23 00:17:35 +0800

    Refactoring: modify parseFiles.js

--- src/parseFiles.js
+++ src/parseFiles.js
@@ -9,51 +9,56 @@ var fs = require('fs'),
 var	encode = "utf8",
 	parser = new xml2js.Parser();
 
-module.exports = {
-	executeParseXml:function(dirPath){
+var executeParseXml = function(dirPath){
+
 			var deferred = new $.Deferred(),
 					count = 0,
 					fileName = "",
 					filePath = "",
 					parseResult = [],
 					tempCount =0;
-
+		
+		readDirAndParseXml(dirPath);
+		return deferred;
+		
+		function readDirAndParseXml(dirPath){
 			fs.readdir(dirPath,function(err,files){
 				if(typeof files === "undefined")
 					files = [];
-				if(err){
-					console.log(err);
-					return;
-				}
+				if(err) throw err;
 				
 				count = files.length;
 				for (var i=0;i<count;i++){
 					fileName = files[i];
-					console.log("Start Parse : " + fileName);		
 					filePath = path.join(dirPath,fileName);
-						fs.readFile(filePath,encode,function(err,file){
-							if(err){
-								console.log(err);
-								return;
-							}
-							parser.parseString(file,function(err,result){
-								if(err){
-									console.log(err);
-									return;
-								}
-							  parseResult.push(result);
-							  tempCount ++;	
-								if(tempCount === count){
-									deferred.resolve(parseResult);			
-								}	
-							});								
-						});		
+					readXmlFile(filePath,encode);
 				}	
 			});
-		return deferred;
-  }
+		}
+
+		function readXmlFile(filePath,encode){
+			fs.readFile(filePath,encode,function(err,file){
+					if(err) throw err;
+					parseXml(file);
+			});		
+		}
+
+		function parseXml(file){	
+			parser.parseString(file,function(err,result){
+					if(err) throw err;
+					parseResult.push(result);
+					tempCount ++;	
+					if(tempCount === count){
+					deferred.resolve(parseResult);			
+					}	
+			});								
+		}
+
 };
 
 
+module.exports = {
+	executeParseXml:executeParseXml
+};
 
 
